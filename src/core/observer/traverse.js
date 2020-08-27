97b42c5bfd86d1180567931e323251cf1bcb1534
CodingWithAlice <48755515+CodingWithAlice@users.noreply.github.com>
2020-08-27 21:49:10 +0800

    add：添加watch侦听属性中的 deep属性遍历方法traverse的说明

--- src/core/observer/traverse.js
+++ src/core/observer/traverse.js
@@ -10,8 +10,9 @@ const seenObjects = new Set()
  * Recursively traverse an object to evoke all converted
  * getters, so that every nested property inside the object
  * is collected as a "deep" dependency.
+ * 作用：深度遍历一个对象/数组，触发每一个值的 getter，方便收集依赖
  */
-export function traverse (val: any) {
+export function trave rse (val: any) {
   _traverse(val, seenObjects)
   seenObjects.clear()
 }
@@ -23,6 +24,7 @@ function _traverse (val: any, seen: SimpleSet) {
     return
   }
   if (val.__ob__) {
+    // 把每个值的对象的 dep 的 id 作为 key
     const depId = val.__ob__.dep.id
     if (seen.has(depId)) {
       return
@@ -30,9 +32,11 @@ function _traverse (val: any, seen: SimpleSet) {
     seen.add(depId)
   }
   if (isA) {
+    // 如果传入的是一个数组，就进行递归调用
     i = val.length
     while (i--) _traverse(val[i], seen)
   } else {
+    // 如果传入的是一个对象，就遍历 key 值调用
     keys = Object.keys(val)
     i = keys.length
     while (i--) _traverse(val[keys[i]], seen)
